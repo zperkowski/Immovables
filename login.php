2e838c0e88842c75fefaca8595eba4390b867cb3
zperkowski <ZdzislawPerkowski@outlook.com>
2018-01-06 12:52:40 +0100

    Passing data to a PHP file

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+        echo $_POST["login"];
+        echo "<br>";
+        echo $_POST["password"];
+    ?>
+</body>
+</html>
+
+
